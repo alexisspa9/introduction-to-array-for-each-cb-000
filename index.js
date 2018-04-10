53a7a3e4dbd3e36f51ee82cf3f712c6a106f8fb4
Alexander Spanos <alexisspa9@gmail.com>
2018-04-10 19:44:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// Add your doToElementsInArray() function here:
+
 function changeCompletely(element, index, array) {
   array[index] = element + '!!!'
 }
+
+// Add your changeCompletely() function here:
 
 function doToElementsInArray(array, callback) {
   array.forEach(callback)
